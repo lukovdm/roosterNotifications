db0bd6c93e7d69289d2bcd9edb0be156aaab7fe1
Luko van der Maas <lukovandermasa@gmail.com>
2014-10-08 11:01:01 +0200

    started on reding page

--- roosterNotification.py
+++ roosterNotification.py
@@ -1,3 +1,7 @@
-import
+import urllib
+from lxml import html
+
+url = "http://gepro.nl/roosters/rooster.php?leerling=1102053&type=Leerlingrooster&afdeling=schooljaar2014-2015_OVERIG&wijzigingen=1&school=1814"
+page = html.s
 
 __author__ = 'luko'
