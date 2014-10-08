5097fdb59b4a280f8213b88598d76fe6ba1dc0e2
Luko van der Maas <lukovandermaas@gmail.com>
2014-10-08 12:45:59 +0200

    improved printing

--- roosterNotification.py
+++ roosterNotification.py
@@ -3,17 +3,23 @@ import urllib2, re
 url = "http://gepro.nl/roosters/rooster.php?leerling=1102053&type=Leerlingrooster&afdeling=schooljaar2014-2015_OVERIG&wijzigingen=1&school=1814"
 htmlPage = urllib2.urlopen(url).read()
 
-pattern = re.compile('class="tableCell(Removed|New)">(y([0-9]+)|[a-z]+|[0-9])')
-for part in re.finditer(pattern, htmlPage):
-    print part.group()
+parts = []
+stage = 0
+pattern = re.compile('class="tableCell(Removed|New)">(y([0-9]+)|[a-z]+)')
+for string in re.finditer(pattern, htmlPage):
+    if string.group(1) == "Removed":
+        parts.append(string.group(2))
+    else:
+        stage += 1
+        if stage == 1:
+            parts.append([string.group(2)])
+        else:
+            parts[-1].append(string.group(2))
+        if stage == 3:
+            stage = 0
 
-
-# done = False
-# index = 0
-# while not done:
-#     index = htmlPage.find('class="tableCellRemoved">vrij', index+1)
-#     if index == -1:
-#         break
-#     print "found one at: " + str(index)
-
-__author__ = 'luko'
+for part in parts:
+    if len(part) == 3:
+        print part[0] + " " + part[1] + " " + part[2]
+    else:
+        print part
