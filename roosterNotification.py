3bf91b01be5c61c8a06e1e03ac7c86dda7bbd59e
Luko van der Maas <lukovandermaas@gmail.com>
2014-10-08 12:58:40 +0200

    added pushbullet

--- roosterNotification.py
+++ roosterNotification.py
@@ -1,11 +1,16 @@
 import urllib2, re
+from pushbullet import PushBullet
 
-url = "http://gepro.nl/roosters/rooster.php?leerling=1102053&type=Leerlingrooster&afdeling=schooljaar2014-2015_OVERIG&wijzigingen=1&school=1814"
+api_key = "v1vPiFeYJ9xD5ueb9OYzoC20zVE8z4m5BPujxa3ey3kdw"
+pb = PushBullet(api_key)
+
+leerlingnummer = 1102053
+
+url = "http://gepro.nl/roosters/rooster.php?leerling=" + str(leerlingnummer) + "&type=Leerlingrooster&afdeling=schooljaar2014-2015_OVERIG&wijzigingen=1&school=1814"
 htmlPage = urllib2.urlopen(url).read()
-
 parts = []
 stage = 0
-pattern = re.compile('class="tableCell(Removed|New)">(y([0-9]+)|[a-z]+)')
+pattern = re.compile('class="tableCell(New|Removed)">(y([0-9]+)|[a-z]+)')
 for string in re.finditer(pattern, htmlPage):
     if string.group(1) == "Removed":
         parts.append(string.group(2))
@@ -18,8 +23,11 @@ for string in re.finditer(pattern, htmlPage):
         if stage == 3:
             stage = 0
 
+text = ""
 for part in parts:
     if len(part) == 3:
-        print part[0] + " " + part[1] + " " + part[2]
+        text += part[0] + " " + part[1] + " " + part[2] + "\n"
     else:
-        print part
+        text += part + "\n"
+
+success, push = pb.push_note("Rooster wijzigingen", text)
