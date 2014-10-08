0bbdef0a9c5e441e29e825d9b7b5057837e46646
Luko van der Maas <lukovandermaas@gmail.com>
2014-10-08 12:24:56 +0200

    working regex pattern for ro0ster

--- roosterNotification.py
+++ roosterNotification.py
@@ -1,7 +1,19 @@
-import urllib
-from lxml import html
+import urllib2, re
 
 url = "http://gepro.nl/roosters/rooster.php?leerling=1102053&type=Leerlingrooster&afdeling=schooljaar2014-2015_OVERIG&wijzigingen=1&school=1814"
-page = html.s
+htmlPage = urllib2.urlopen(url).read()
+
+pattern = re.compile('class="tableCell(Removed|New)">(y([0-9]+)|[a-z]+|[0-9])')
+for part in re.finditer(pattern, htmlPage):
+    print part.group(2)
+
+
+# done = False
+# index = 0
+# while not done:
+#     index = htmlPage.find('class="tableCellRemoved">vrij', index+1)
+#     if index == -1:
+#         break
+#     print "found one at: " + str(index)
 
 __author__ = 'luko'
